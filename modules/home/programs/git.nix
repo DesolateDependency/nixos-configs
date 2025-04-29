cb043d5c00cea74ecff0ddc76258438db7386ea7
DesDepy <desolatedependency@gmail.com>
2025-04-30 00:53:49 +0200

    add git aliases

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }: {
   programs.git = {
     enable = true;
-      userName = "DesDepy";
-      userEmail = "desolatedependency@gmail.com";
-      extraConfig = {
-        init.defaultBranch = "main";
-      };
+    userName = "DesDepy";
+    userEmail = "desolatedependency@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+    aliases = {
+      graph = "log --graph --date-order --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+      fullgraph = "log --graph --date-order --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+    };
   };
 }
