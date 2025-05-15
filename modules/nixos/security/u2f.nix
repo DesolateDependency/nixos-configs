3d032fd7cc4358341830238cd20f5fa3b9639554
DesDepy <desolatedependency@gmail.com>
2025-05-16 00:03:57 +0200

    cleanup Yubikey implementation

--- modules/nixos/security/u2f.nix
+++ modules/nixos/security/u2f.nix
@@ -1,24 +1,27 @@
 { pkgs, lib, ... }:
 
   let
-    linux-pam = pkgs.linux-pam;
-    pam_u2f = pkgs.pam_u2f;
+    pam_unix = "${pkgs.linux-pam}/lib/security/pam_unix.so";
+    pam_u2f = "${pkgs.pam_u2f}/lib/security/pam_u2f.so";
   in {
 
+    # Sudo possible with only Yubikey
     security.pam.services.sudo.text = pkgs.lib.mkDefault (pkgs.lib.mkBefore ''
       # Extra authentication management.
-      auth sufficient ${pam_u2f}/lib/security/pam_u2f.so cue
+      auth sufficient ${pam_u2f} cue
     '');
 
+    # Login needs password and Yubikey
     security.pam.services.login.text = pkgs.lib.mkDefault (pkgs.lib.mkBefore ''
       # Extra authentication management.
-      auth required ${linux-pam}/lib/security/pam_unix.so
-      auth required ${pam_u2f}/lib/security/pam_u2f.so cue
+      auth required ${pam_unix}
+      auth required ${pam_u2f} cue
     '');
 
+    # KDE (KScreenLocker) in needs password and Yubikey
     security.pam.services.kde.text = pkgs.lib.mkDefault (pkgs.lib.mkBefore ''
       # Extra authentication management.
-      auth required ${linux-pam}/lib/security/pam_unix.so
-      auth required ${pam_u2f}/lib/security/pam_u2f.so cue
+      auth required ${pam_unix}
+      auth required ${pam_u2f} cue
     '');
   }
